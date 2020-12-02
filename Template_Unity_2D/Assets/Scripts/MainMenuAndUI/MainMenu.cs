d01e2619533f07056e010f552160951aab80f735
olvanyr <olvanyr@gmail.com>
2020-12-02 16:52:11 +0100

    more work on the menu

---
+++ Template_Unity_2D/Assets/Scripts/MainMenuAndUI/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public GameObject settingsWindow;
+
+    public GameObject firstSelectedButton, firstSettingSelectedButton, optionClosedButton;
+
+    public AudioClip highlightSound;
+    public AudioClip onClickSound;
+
+    public AudioMixer audioMixer;
+
+    private void Start()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstSelectedButton);
+    }
+
+    public void StartGameButton() //this one is obsolote, now I use a levelLoader
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void SettingsButton()
+    {
+        settingsWindow.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(firstSettingSelectedButton);
+    }
+    public void CloseSettingsButton()
+    {
+        settingsWindow.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionClosedButton);
+    }
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+    
+    public void OnClickSound()
+    {
+        AudioManager.instance.PlayClipAt(onClickSound, "Sound", transform.position);
+    }
+    public void HighlightSound()
+    {
+        AudioManager.instance.PlayClipAt(highlightSound, "Sound", transform.position);
+    }
+}
---
+++ Template_Unity_2D/Assets/Scripts/MainMenuAndUI/SettingsMenu.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Linq;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public AudioMixer audioMixer;
+
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider soundSlider;
+
+    public Toggle fullscreenToggl;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Awake()
+    {
+        //Load fullscreen
+        int isFullscreen = PlayerPrefs.GetInt("Fullscreen");
+
+        if (isFullscreen == 1)
+        {
+            SetFullScreen(true);
+            fullscreenToggl.isOn = true;
+        }
+        else
+        {
+            SetFullScreen(false);
+            fullscreenToggl.isOn = false;
+        }
+
+        //load resolution
+        resolutionDropdown.onValueChanged.AddListener(new UnityAction<int>(indexer =>
+        {
+            PlayerPrefs.SetInt("Resolution", resolutionDropdown.value);
+            PlayerPrefs.Save();
+        }));
+        
+    }
+
+    public void Start()
+    {
+        float masterVolume = PlayerPrefs.GetFloat("VolumeMaster", 0);
+        masterSlider.value = masterVolume;
+        audioMixer.SetFloat("VolumeMaster", masterVolume);
+        float musicVolume = PlayerPrefs.GetFloat("VolumeMusic", 0);
+        musicSlider.value = musicVolume;
+        audioMixer.SetFloat("VolumeMusic", musicVolume);
+        float soundVolume = PlayerPrefs.GetFloat("VolumeSound", 0);
+        soundSlider.value = soundVolume;
+        audioMixer.SetFloat("VolumeSound", soundVolume);
+
+        //get the resolution of the screen
+        resolutions = Screen.resolutions.Select(Resolution => new Resolution { width = Resolution.width, height = Resolution.height }).Distinct().ToArray(); //avoid duplication into the resoluion array
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = PlayerPrefs.GetInt("Resolution", currentResolutionIndex);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+
+    public void SetMasterVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("VolumeMaster", volume);
+        audioMixer.SetFloat("VolumeMaster", volume);
+    }
+    public void SetSoundVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("VolumeSound", volume);
+        audioMixer.SetFloat("VolumeSound", volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("VolumeMusic", volume);
+        audioMixer.SetFloat("VolumeMusic", volume);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+
+        if (isFullScreen)
+        {
+            PlayerPrefs.SetInt("Fullscreen", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Fullscreen", 0);
+        }
+    }
+
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void ClearSavedData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
